5037eff255c10faf19789f12c73f8ef99aa3885f
FreeAndWithBeer <30539567+FreeAndWithBeer@users.noreply.github.com>
2023-08-27 19:25:21 -0400

    Created XElementSimpleStreamer.

---
+++ FOSStrich.BCL.Opinions.Test/Xml/Linq/XElementSimpleStreamer.cs
@@ -0,0 +1,57 @@
+﻿namespace FOSStrich.Xml.Linq;
+
+using System.Xml;
+using System.Xml.Linq;
+
+public class XElementSimpleStreamerTests
+{
+    [Fact]
+    public void Basic()
+    {
+        XElement[] results;
+
+        results = XElementSimpleStreamer.Stream(Reader(string.Empty), "Child").ToArray();
+        results.Length.Should().Be(3);
+
+        results[0].Value.Should().Be("A");
+        results[1].Value.Should().Be("B");
+
+        results[2].Elements().ToArray()[0].Value.Should().Be("AA");
+        results[2].Elements().ToArray()[1].Value.Should().Be("BB");
+
+        results = XElementSimpleStreamer.Stream(Reader(string.Empty), "Grandchild").ToArray();
+        results.Length.Should().Be(2);
+
+        results[0].Value.Should().Be("AA");
+        results[1].Value.Should().Be("BB");
+
+        const string ns = "Foobar";
+
+        results = XElementSimpleStreamer.Stream(Reader(ns), XName.Get("Grandchild", ns)).ToArray();
+        results.Length.Should().Be(2);
+
+        results[0].Value.Should().Be("AA");
+        results[1].Value.Should().Be("BB");
+    }
+
+    [Fact]
+    public void Exceptions()
+    {
+        Action act;
+
+        act = () => XElementSimpleStreamer.Stream(null, "Foobar").ToArray();
+        act.Should().ThrowExactly<ArgumentNullException>();
+
+        act = () => XElementSimpleStreamer.Stream(Reader(string.Empty), null).ToArray();
+        act.Should().ThrowExactly<ArgumentNullException>();
+    }
+
+    private static XmlReader Reader(XNamespace ns) =>
+        new XElement("Root",
+            new XElement(ns + "Child", "A"),
+            new XElement(ns + "Child", "B"),
+            new XElement(ns + "Child",
+                new XElement(ns + "Grandchild", "AA"),
+                new XElement(ns + "Grandchild", "BB")))
+        .CreateReader();
+}
---
+++ FOSStrich.BCL.Opinions/Xml/Linq/XElementSimpleStreamer.cs
@@ -0,0 +1,45 @@
+﻿namespace FOSStrich.Xml.Linq;
+
+using System.Xml;
+using System.Xml.Linq;
+
+public static class XElementSimpleStreamer
+{
+    /// <summary>
+    /// Provides a simple (primitive) way to stream XElements from an XmlReader.
+    /// 
+    /// Code adapted from: http://blogs.msdn.com/xmlteam/archive/2007/03/24/streaming-with-linq-to-xml-part-2.aspx.
+    /// </summary>
+    /// <param name="reader">An XmlReader created using one of the many XmlReader.Create overloads.</param>
+    /// <param name="elementName">
+    /// When any element is found in the source xml document, if the element's name matches the elementName
+    /// parameter, that element is read into an XElement and yielded into the enumeration.
+    /// </param>
+    public static IEnumerable<XElement> Stream(XmlReader reader, XName elementName)
+    {
+        if (reader == null)
+            throw new ArgumentNullException(nameof(reader));
+
+        if (elementName == null)
+            throw new ArgumentNullException(nameof(elementName));
+
+        reader.MoveToContent();
+
+        bool yielded;
+
+        do
+        {
+            // We track yielded in order to short-circuit reader.Read() because XElement.ReadFrom will
+            // have already advanced the XmlReader to the next Node.
+            yielded = false;
+
+            if (reader.NodeType == XmlNodeType.Element
+                && XName.Get(reader.LocalName, reader.NamespaceURI) == elementName)
+            {
+                yield return (XElement)XNode.ReadFrom(reader);
+                yielded = true;
+            }
+        }
+        while (yielded || reader.Read());
+    }
+}
