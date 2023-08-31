ab34f52778537e485db127817cc7c5d7b2b42254
FreeAndWithBeer <30539567+FreeAndWithBeer@users.noreply.github.com>
2023-08-31 18:00:07 -0400

    Added PathX helper methods for working with CallerFilePath.

---
+++ FOSStrich.BCL.Opinions.Test/IO/PathX.cs
@@ -0,0 +1,32 @@
+﻿namespace FOSStrich.IO;
+
+public class PathXTests
+{
+    [Fact]
+    public void Basic()
+    {
+        PathX.GetDirectoryNameOfCallerFilePath(@"C:\Foo\A.txt").Should().Be(@"C:\Foo");
+        PathX.GetDirectoryNameOfCallerFilePath(@"C:\Foo\Bar\A.txt").Should().Be(@"C:\Foo\Bar");
+
+        PathX.GetFullPathRelativeToCallerFilePath("B.txt", @"C:\Foo\A.txt").Should().Be(@"C:\Foo\B.txt");
+        PathX.GetFullPathRelativeToCallerFilePath(@"Bar\B.txt", @"C:\Foo\A.txt").Should().Be(@"C:\Foo\Bar\B.txt");
+    }
+
+    [Fact]
+    public void Exceptions()
+    {
+        Action act;
+
+        act = () => PathX.GetDirectoryNameOfCallerFilePath(null);
+        act.Should().ThrowExactly<ArgumentNullException>();
+
+        act = () => PathX.GetDirectoryNameOfCallerFilePath(@"NotRooted.txt");
+        act.Should().ThrowExactly<ArgumentOutOfRangeException>();
+
+        act = () => PathX.GetFullPathRelativeToCallerFilePath(null);
+        act.Should().ThrowExactly<ArgumentNullException>();
+
+        act = () => PathX.GetFullPathRelativeToCallerFilePath(@"C:\Rooted.txt");
+        act.Should().ThrowExactly<ArgumentOutOfRangeException>();
+    }
+}
---
+++ FOSStrich.BCL.Opinions/IO/PathX.cs
@@ -0,0 +1,32 @@
+﻿namespace FOSStrich.IO;
+
+using System.IO;
+using System.Runtime.CompilerServices;
+
+public static class PathX
+{
+    public static string GetDirectoryNameOfCallerFilePath([CallerFilePath] string callerFilePath = null)
+    {
+        if (callerFilePath == null)
+            throw new ArgumentNullException(nameof(callerFilePath));
+
+        if (!Path.IsPathRooted(callerFilePath))
+            throw new ArgumentOutOfRangeException(nameof(callerFilePath));
+
+        return Path.GetDirectoryName(callerFilePath);
+    }
+
+    public static string GetFullPathRelativeToCallerFilePath(string relativePath, [CallerFilePath] string callerFilePath = null)
+    {
+        if (relativePath == null)
+            throw new ArgumentNullException(nameof(relativePath));
+
+        if (Path.IsPathRooted(relativePath))
+            throw new ArgumentOutOfRangeException(nameof(relativePath));
+
+        // We use our helper method here because of its validations (DRY).
+        string directory = GetDirectoryNameOfCallerFilePath(callerFilePath);
+
+        return Path.Combine(directory, relativePath);
+    }
+}
