94c0fa404984fcd2fb6009d4cb0ac6ba6dabbe69
FreeAndWithBeer <30539567+FreeAndWithBeer@users.noreply.github.com>
2025-09-09 11:37:13 -0400

    Added TypeX helpers for a gap in System.Numerics and for Type to C# keyword mapping.

---
+++ NorthSouthSystems.BCL.Opinions.Test/TypeX.cs
@@ -0,0 +1,27 @@
+﻿namespace NorthSouthSystems;
+
+public class TypeXTests
+{
+    [Fact]
+    public void IsFloatingPoint()
+    {
+        typeof(double).IsFloatingPoint().Should().BeTrue();
+        typeof(int).IsFloatingPoint().Should().BeFalse();
+    }
+
+    [Fact]
+    public void IsIntegral()
+    {
+        typeof(double).IsIntegral().Should().BeFalse();
+        typeof(int).IsIntegral().Should().BeTrue();
+    }
+
+    [Fact]
+    public void CSharpKeywordsByType()
+    {
+        TypeX.CSharpKeywordsByType[typeof(double)].Should().Be("double");
+        TypeX.CSharpKeywordsByType[typeof(int)].Should().Be("int");
+
+        TypeX.CSharpKeywordsByType.ContainsKey(typeof(TypeX)).Should().BeFalse();
+    }
+}
---
+++ NorthSouthSystems.BCL.Opinions/TypeX.cs
@@ -0,0 +1,73 @@
+﻿namespace NorthSouthSystems;
+
+using System.Collections.Immutable;
+using System.Numerics;
+
+public static class TypeX
+{
+    // Unfortunately, there is no simpler method to determine this. All Systems.Numerics interfaces
+    // are recursive generics (i.e. IInterface<T> where T : IInterface<T>), so they can't be used
+    // with "is" or "as" operators on instances or with Type.IsAssignable for Types (without Reflection).
+    public static bool IsFloatingPoint(this Type type)
+    {
+        ArgumentNullException.ThrowIfNull(type);
+
+        return FloatingPointTypes.Contains(type);
+    }
+
+    public static ImmutableHashSet<Type> FloatingPointTypes { get; } =
+    [
+        typeof(Half),
+        typeof(float),
+        typeof(double),
+        typeof(decimal)
+    ];
+
+    // Unfortunately, there is no simpler method to determine this. All Systems.Numerics interfaces
+    // are recursive generics (i.e. IInterface<T> where T : IInterface<T>), so they can't be used
+    // with "is" or "as" operators on instances or with Type.IsAssignable for Types (without Reflection).
+    public static bool IsIntegral(this Type type)
+    {
+        ArgumentNullException.ThrowIfNull(type);
+
+        return IntegralTypes.Contains(type);
+    }
+
+    public static ImmutableHashSet<Type> IntegralTypes { get; } =
+    [
+        typeof(byte),
+        typeof(sbyte),
+        typeof(short),
+        typeof(ushort),
+        typeof(int),
+        typeof(uint),
+        typeof(long),
+        typeof(ulong),
+
+        typeof(Int128),
+        typeof(BigInteger)
+    ];
+
+    public static ImmutableDictionary<Type, string> CSharpKeywordsByType { get; } =
+        new Dictionary<Type, string>
+        {
+            [typeof(bool)] = "bool",
+            [typeof(byte)] = "byte",
+            [typeof(sbyte)] = "sbyte",
+            [typeof(short)] = "short",
+            [typeof(ushort)] = "ushort",
+            [typeof(int)] = "int",
+            [typeof(uint)] = "uint",
+            [typeof(long)] = "long",
+            [typeof(ulong)] = "ulong",
+            [typeof(float)] = "float",
+            [typeof(double)] = "double",
+            [typeof(decimal)] = "decimal",
+
+            [typeof(char)] = "char",
+            [typeof(string)] = "string",
+
+            [typeof(object)] = "object",
+        }
+        .ToImmutableDictionary();
+}
