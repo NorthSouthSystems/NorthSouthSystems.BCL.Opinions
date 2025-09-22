877a8c109a51f9fb69140ea3b681c62b1f773bbe
FreeAndWithBeer <30539567+FreeAndWithBeer@users.noreply.github.com>
2025-09-22 14:55:07 -0400

    Added PropertyInfoX GetGetterReturnTypeOrThrow method. Broke binary compatibility by adding "bool includeNonPublic = false" default parameter to all PropertyInfoX public methods. Incremented major version accordingly.

--- NorthSouthSystems.BCL.Opinions.Tests/Reflection/T_PropertyInfoX.cs
+++ NorthSouthSystems.BCL.Opinions.Tests/Reflection/T_PropertyInfoX.cs
@@ -1,7 +1,7 @@
 ﻿// Adapted and simplified from a ChatGPT conversation on 2025-08-21.
 using NorthSouthSystems.Reflection;
 
-public class T_PropertyInfoX
+public sealed class T_PropertyInfoX
 {
     public sealed class TheClass
     {
@@ -12,10 +12,11 @@ public class T_PropertyInfoX
         public string NullStringProperty => null;
         public double? NullDoubleProperty => null;
 
+        internal string NonPublicStringProperty => "nonpublic";
+
         public OtherClass Other => new();
 
         // For Exceptions.
-        internal string NonPublicStringProperty => "nonpublic";
         public string SetterOnlyStringProperty { set { } }
     }
 
@@ -27,6 +28,8 @@ public class T_PropertyInfoX
 
         public string NullStringProperty => null;
         public double? NullDoubleProperty => null;
+
+        internal string NonPublicStringProperty => "public-not";
 
         public AnotherClass Another => new();
     }
@@ -51,105 +54,195 @@ public class T_PropertyInfoX
     // For Exceptions.
     public struct TheStruct { public string StringProperty => "struct"; }
 
-    [Fact]
-    public void GetValueComiled_SinglePart()
+    public sealed class GetValueCompiled
     {
-        object result;
+        [Fact]
+        public void OneSegment()
+        {
+            object result;
 
-        result = PropertyInfoX.GetValueCompiled(new TheClass(), nameof(TheClass.StringProperty));
-        result.Should().BeOfType<string>().Which.Should().Be(new TheClass().StringProperty);
+            result = PropertyInfoX.GetValueCompiled(new TheClass(), nameof(TheClass.StringProperty));
+            result.Should().BeOfType<string>().Which.Should().Be(new TheClass().StringProperty);
 
-        result = PropertyInfoX.GetValueCompiled(new TheClass(), nameof(TheClass.IntProperty));
-        result.Should().BeOfType<int>().Which.Should().Be(new TheClass().IntProperty);
+            result = PropertyInfoX.GetValueCompiled(new TheClass(), nameof(TheClass.IntProperty));
+            result.Should().BeOfType<int>().Which.Should().Be(new TheClass().IntProperty);
 
-        result = PropertyInfoX.GetValueCompiled(new TheClass(), nameof(TheClass.NullableDoubleProperty));
-        result.Should().BeOfType<double>().Which.Should().Be(new TheClass().NullableDoubleProperty);
+            result = PropertyInfoX.GetValueCompiled(new TheClass(), nameof(TheClass.NullableDoubleProperty));
+            result.Should().BeOfType<double>().Which.Should().Be(new TheClass().NullableDoubleProperty);
 
-        result = PropertyInfoX.GetValueCompiled(new TheClass(), nameof(TheClass.NullStringProperty));
-        result.Should().BeNull();
+            result = PropertyInfoX.GetValueCompiled(new TheClass(), nameof(TheClass.NullStringProperty));
+            result.Should().BeNull();
 
-        result = PropertyInfoX.GetValueCompiled(new TheClass(), nameof(TheClass.NullDoubleProperty));
-        result.Should().BeNull();
+            result = PropertyInfoX.GetValueCompiled(new TheClass(), nameof(TheClass.NullDoubleProperty));
+            result.Should().BeNull();
 
-        // Derived
+            result = PropertyInfoX.GetValueCompiled(new TheClass(), nameof(TheClass.NonPublicStringProperty), true);
+            result.Should().BeOfType<string>().Which.Should().Be(new TheClass().NonPublicStringProperty);
 
-        result = PropertyInfoX.GetValueCompiled(new TheDerived(), nameof(TheDerived.StringProperty));
-        result.Should().BeOfType<string>().Which.Should().Be(new TheDerived().StringProperty);
+            // Derived
+
+            result = PropertyInfoX.GetValueCompiled(new TheDerived(), nameof(TheDerived.StringProperty));
+            result.Should().BeOfType<string>().Which.Should().Be(new TheDerived().StringProperty);
+        }
+
+        [Fact]
+        public void TwoSegments()
+        {
+            object result;
+
+            result = PropertyInfoX.GetValueCompiled(new TheClass(), $"{nameof(TheClass.Other)}.{nameof(OtherClass.StringProperty)}");
+            result.Should().BeOfType<string>().Which.Should().Be(new OtherClass().StringProperty);
+
+            result = PropertyInfoX.GetValueCompiled(new TheClass(), $"{nameof(TheClass.Other)}.{nameof(OtherClass.IntProperty)}");
+            result.Should().BeOfType<int>().Which.Should().Be(new OtherClass().IntProperty);
+
+            result = PropertyInfoX.GetValueCompiled(new TheClass(), $"{nameof(TheClass.Other)}.{nameof(OtherClass.NullableDoubleProperty)}");
+            result.Should().BeOfType<double>().Which.Should().Be(new OtherClass().NullableDoubleProperty);
+
+            result = PropertyInfoX.GetValueCompiled(new TheClass(), $"{nameof(TheClass.Other)}.{nameof(OtherClass.NullStringProperty)}");
+            result.Should().BeNull();
+
+            result = PropertyInfoX.GetValueCompiled(new TheClass(), $"{nameof(TheClass.Other)}.{nameof(OtherClass.NullDoubleProperty)}");
+            result.Should().BeNull();
+
+            result = PropertyInfoX.GetValueCompiled(new TheClass(), $"{nameof(TheClass.Other)}.{nameof(OtherClass.NonPublicStringProperty)}", true);
+            result.Should().BeOfType<string>().Which.Should().Be(new OtherClass().NonPublicStringProperty);
+
+            // Derived
+
+            result = PropertyInfoX.GetValueCompiled(new TheDerived(), $"{nameof(TheDerived.Other)}.{nameof(OtherClass.StringProperty)}");
+            result.Should().BeOfType<string>().Which.Should().Be(new OtherClass().StringProperty);
+
+            result = PropertyInfoX.GetValueCompiled(new TheDerived(), $"{nameof(TheDerived.Another)}.{nameof(AnotherClass.StringProperty)}");
+            result.Should().BeOfType<string>().Which.Should().Be(new AnotherClass().StringProperty);
+        }
+
+        [Fact]
+        public void ThreeSegments()
+        {
+            object result;
+
+            result = PropertyInfoX.GetValueCompiled(new TheClass(), $"{nameof(TheClass.Other)}.{nameof(OtherClass.Another)}.{nameof(AnotherClass.StringProperty)}");
+            result.Should().BeOfType<string>().Which.Should().Be(new AnotherClass().StringProperty);
+
+            // Derived
+
+            result = PropertyInfoX.GetValueCompiled(new TheDerived(), $"{nameof(TheDerived.Other)}.{nameof(OtherClass.Another)}.{nameof(AnotherClass.StringProperty)}");
+            result.Should().BeOfType<string>().Which.Should().Be(new AnotherClass().StringProperty);
+        }
+
+        [Fact]
+        public void Exceptions()
+        {
+            Action act;
+
+            act = static () => PropertyInfoX.GetValueCompiled(null, nameof(TheClass.StringProperty));
+            act.Should().ThrowExactly<ArgumentNullException>().Which.ParamName.Should().Be("obj");
+
+            act = static () => PropertyInfoX.GetValueCompiled(new TheClass(), null);
+            act.Should().ThrowExactly<ArgumentNullException>().Which.ParamName.Should().Be("propertyPath");
+
+            act = static () => PropertyInfoX.GetValueCompiled(new TheClass(), string.Empty);
+            act.Should().ThrowExactly<ArgumentException>().Which.ParamName.Should().Be("propertyPath");
+
+            act = static () => PropertyInfoX.GetValueCompiled(new TheStruct(), nameof(TheStruct.StringProperty));
+            act.Should().ThrowExactly<ArgumentException>().WithMessage("Value types are not allowed*");
+        }
     }
 
-    [Fact]
-    public void GetValueComiled_TwoPart()
+    public sealed class GetGetterReturnTypeOrThrow
     {
-        object result;
+        [Fact]
+        public void OneSegment()
+        {
+            object result;
 
-        result = PropertyInfoX.GetValueCompiled(new TheClass(), $"{nameof(TheClass.Other)}.{nameof(OtherClass.StringProperty)}");
-        result.Should().BeOfType<string>().Which.Should().Be(new OtherClass().StringProperty);
+            result = PropertyInfoX.GetGetterReturnTypeOrThrow(typeof(TheClass), nameof(TheClass.StringProperty));
+            result.Should().Be(typeof(string));
 
-        result = PropertyInfoX.GetValueCompiled(new TheClass(), $"{nameof(TheClass.Other)}.{nameof(OtherClass.IntProperty)}");
-        result.Should().BeOfType<int>().Which.Should().Be(new OtherClass().IntProperty);
+            result = PropertyInfoX.GetGetterReturnTypeOrThrow(typeof(TheClass), nameof(TheClass.IntProperty));
+            result.Should().Be(typeof(int));
 
-        result = PropertyInfoX.GetValueCompiled(new TheClass(), $"{nameof(TheClass.Other)}.{nameof(OtherClass.NullableDoubleProperty)}");
-        result.Should().BeOfType<double>().Which.Should().Be(new OtherClass().NullableDoubleProperty);
+            result = PropertyInfoX.GetGetterReturnTypeOrThrow(typeof(TheClass), nameof(TheClass.NullableDoubleProperty));
+            result.Should().Be(typeof(double?));
 
-        result = PropertyInfoX.GetValueCompiled(new TheClass(), $"{nameof(TheClass.Other)}.{nameof(OtherClass.NullStringProperty)}");
-        result.Should().BeNull();
+            result = PropertyInfoX.GetGetterReturnTypeOrThrow(typeof(TheClass), nameof(TheClass.NullStringProperty));
+            result.Should().Be(typeof(string));
 
-        result = PropertyInfoX.GetValueCompiled(new TheClass(), $"{nameof(TheClass.Other)}.{nameof(OtherClass.NullDoubleProperty)}");
-        result.Should().BeNull();
+            result = PropertyInfoX.GetGetterReturnTypeOrThrow(typeof(TheClass), nameof(TheClass.NullDoubleProperty));
+            result.Should().Be(typeof(double?));
 
-        // Derived
+            result = PropertyInfoX.GetGetterReturnTypeOrThrow(typeof(TheClass), nameof(TheClass.NonPublicStringProperty), true);
+            result.Should().Be(typeof(string));
 
-        result = PropertyInfoX.GetValueCompiled(new TheDerived(), $"{nameof(TheDerived.Other)}.{nameof(OtherClass.StringProperty)}");
-        result.Should().BeOfType<string>().Which.Should().Be(new OtherClass().StringProperty);
+            // Derived
 
-        result = PropertyInfoX.GetValueCompiled(new TheDerived(), $"{nameof(TheDerived.Another)}.{nameof(AnotherClass.StringProperty)}");
-        result.Should().BeOfType<string>().Which.Should().Be(new AnotherClass().StringProperty);
+            result = PropertyInfoX.GetGetterReturnTypeOrThrow(typeof(TheDerived), nameof(TheDerived.StringProperty));
+            result.Should().Be(typeof(string));
+        }
+
+        [Fact]
+        public void TwoSegments()
+        {
+            object result;
+
+            result = PropertyInfoX.GetGetterReturnTypeOrThrow(typeof(TheClass), $"{nameof(TheClass.Other)}.{nameof(OtherClass.StringProperty)}");
+            result.Should().Be(typeof(string));
+
+            result = PropertyInfoX.GetGetterReturnTypeOrThrow(typeof(TheClass), $"{nameof(TheClass.Other)}.{nameof(OtherClass.IntProperty)}");
+            result.Should().Be(typeof(int));
+
+            result = PropertyInfoX.GetGetterReturnTypeOrThrow(typeof(TheClass), $"{nameof(TheClass.Other)}.{nameof(OtherClass.NullableDoubleProperty)}");
+            result.Should().Be(typeof(double?));
+
+            result = PropertyInfoX.GetGetterReturnTypeOrThrow(typeof(TheClass), $"{nameof(TheClass.Other)}.{nameof(OtherClass.NullStringProperty)}");
+            result.Should().Be(typeof(string));
+
+            result = PropertyInfoX.GetGetterReturnTypeOrThrow(typeof(TheClass), $"{nameof(TheClass.Other)}.{nameof(OtherClass.NullDoubleProperty)}");
+            result.Should().Be(typeof(double?));
+
+            result = PropertyInfoX.GetGetterReturnTypeOrThrow(typeof(TheClass), $"{nameof(TheClass.Other)}.{nameof(OtherClass.NonPublicStringProperty)}", true);
+            result.Should().Be(typeof(string));
+
+            // Derived
+
+            result = PropertyInfoX.GetGetterReturnTypeOrThrow(typeof(TheDerived), $"{nameof(TheDerived.Other)}.{nameof(OtherClass.StringProperty)}");
+            result.Should().Be(typeof(string));
+
+            result = PropertyInfoX.GetGetterReturnTypeOrThrow(typeof(TheDerived), $"{nameof(TheDerived.Another)}.{nameof(AnotherClass.StringProperty)}");
+            result.Should().Be(typeof(string));
+        }
+
+        [Fact]
+        public void ThreeSegments()
+        {
+            object result;
+
+            result = PropertyInfoX.GetGetterReturnTypeOrThrow(typeof(TheClass), $"{nameof(TheClass.Other)}.{nameof(OtherClass.Another)}.{nameof(AnotherClass.StringProperty)}");
+            result.Should().Be(typeof(string));
+
+            // Derived
+
+            result = PropertyInfoX.GetGetterReturnTypeOrThrow(typeof(TheDerived), $"{nameof(TheDerived.Other)}.{nameof(OtherClass.Another)}.{nameof(AnotherClass.StringProperty)}");
+            result.Should().Be(typeof(string));
+        }
     }
 
-    [Fact]
-    public void GetValueComiled_ThreePart()
+    public sealed class GetGetterMethodOrThrow
     {
-        object result;
+        [Fact]
+        public void Exceptions()
+        {
+            Action act;
 
-        result = PropertyInfoX.GetValueCompiled(new TheClass(), $"{nameof(TheClass.Other)}.{nameof(OtherClass.Another)}.{nameof(AnotherClass.StringProperty)}");
-        result.Should().BeOfType<string>().Which.Should().Be(new AnotherClass().StringProperty);
+            act = static () => PropertyInfoX.GetValueCompiled(new TheClass(), "DoesNotExist");
+            act.Should().ThrowExactly<ArgumentException>();
 
-        // Derived
+            act = static () => PropertyInfoX.GetValueCompiled(new TheClass(), nameof(TheClass.NonPublicStringProperty));
+            act.Should().ThrowExactly<ArgumentException>();
 
-        result = PropertyInfoX.GetValueCompiled(new TheDerived(), $"{nameof(TheDerived.Other)}.{nameof(OtherClass.Another)}.{nameof(AnotherClass.StringProperty)}");
-        result.Should().BeOfType<string>().Which.Should().Be(new AnotherClass().StringProperty);
-    }
-
-    [Fact]
-    public void GetValueCompiled_Exceptions()
-    {
-        Action act;
-
-        act = static () => PropertyInfoX.GetValueCompiled(null, nameof(TheClass.StringProperty));
-        act.Should().ThrowExactly<ArgumentNullException>().Which.ParamName.Should().Be("obj");
-
-        act = static () => PropertyInfoX.GetValueCompiled(new TheClass(), null);
-        act.Should().ThrowExactly<ArgumentNullException>().Which.ParamName.Should().Be("propertyPath");
-
-        act = static () => PropertyInfoX.GetValueCompiled(new TheClass(), string.Empty);
-        act.Should().ThrowExactly<ArgumentException>().Which.ParamName.Should().Be("propertyPath");
-
-        act = static () => PropertyInfoX.GetValueCompiled(new TheStruct(), nameof(TheStruct.StringProperty));
-        act.Should().ThrowExactly<ArgumentException>().WithMessage("Value types are not allowed*");
-    }
-
-    [Fact]
-    public void GetGetterMethodOrThrow_Exceptions()
-    {
-        Action act;
-
-        act = static () => PropertyInfoX.GetValueCompiled(new TheClass(), "DoesNotExist");
-        act.Should().ThrowExactly<ArgumentException>();
-
-        act = static () => PropertyInfoX.GetValueCompiled(new TheClass(), nameof(TheClass.NonPublicStringProperty));
-        act.Should().ThrowExactly<ArgumentException>();
-
-        act = static () => PropertyInfoX.GetValueCompiled(new TheClass(), nameof(TheClass.SetterOnlyStringProperty));
-        act.Should().ThrowExactly<ArgumentException>();
+            act = static () => PropertyInfoX.GetValueCompiled(new TheClass(), nameof(TheClass.SetterOnlyStringProperty));
+            act.Should().ThrowExactly<ArgumentException>();
+        }
     }
 }
--- NorthSouthSystems.BCL.Opinions/Reflection/PropertyInfoX.cs
+++ NorthSouthSystems.BCL.Opinions/Reflection/PropertyInfoX.cs
@@ -8,9 +8,9 @@ using System.Reflection;
 public static class PropertyInfoX
 {
     private static readonly ConcurrentDictionary<GetterCacheKey, Lazy<Func<object, object>>> _getterCache = new();
-    private sealed record GetterCacheKey(Type ObjType, string PropertyPath);
+    private sealed record GetterCacheKey(Type ObjType, string PropertyPath, bool IncludeNonPublic);
 
-    public static object GetValueCompiled(object obj, string propertyPath)
+    public static object GetValueCompiled(object obj, string propertyPath, bool includeNonPublic = false)
     {
         ArgumentNullException.ThrowIfNull(obj);
         ArgumentException.ThrowIfNullOrEmpty(propertyPath);
@@ -18,30 +18,55 @@ public static class PropertyInfoX
         var objType = obj.GetType();
         ValidateObjType(objType);
 
-        var cacheKey = new GetterCacheKey(objType, propertyPath);
+        var cacheKey = new GetterCacheKey(objType, propertyPath, includeNonPublic);
         var getter = _getterCache.GetOrAdd(cacheKey, ck => new(() => CompileGetter(ck)));
 
         return getter.Value(obj);
     }
 
-    public static MethodInfo GetGetterMethodOrThrow(Type objType, string propertyName)
+    public static Type GetGetterReturnTypeOrThrow(Type objType, string propertyPath, bool includeNonPublic = false)
     {
-        ValidateObjType(objType);
+        ArgumentNullException.ThrowIfNull(objType);
+        ArgumentException.ThrowIfNullOrEmpty(propertyPath);
+
+        var propertyPathSegments = SplitPropertyPath(propertyPath);
+
+        var currentType = objType;
+
+        foreach (string propertyName in propertyPathSegments)
+        {
+            var getter = GetGetterMethodOrThrow(currentType, propertyName, includeNonPublic);
+
+            currentType = getter.ReturnType;
+        }
+
+        return currentType;
+    }
+
+    public static MethodInfo GetGetterMethodOrThrow(Type objType, string propertyName, bool includeNonPublic = false)
+    {
+        ArgumentNullException.ThrowIfNull(objType);
         ArgumentException.ThrowIfNullOrEmpty(propertyName);
 
-        const BindingFlags flattenFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.FlattenHierarchy;
-        const BindingFlags declaredFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly;
+        var flattenFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.FlattenHierarchy;
+        var declaredFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly;
+
+        if (includeNonPublic)
+        {
+            flattenFlags |= BindingFlags.NonPublic;
+            declaredFlags |= BindingFlags.NonPublic;
+        }
 
         var property = objType.GetProperty(propertyName, flattenFlags)
-            ?? throw new ArgumentException($"Public property '{propertyName}' not found on {objType}.", nameof(propertyName));
+            ?? throw new ArgumentException($"Property '{propertyName}' not found on {objType} with the requested accessibility.", nameof(propertyName));
 
         // We must get the PropertyInfo from the DeclaringType or else Expression.Property will throw:
         // "System.ArgumentException : The method '...' is not a property accessor (Parameter 'propertyAccessor')"
         if (property.DeclaringType != objType && property.DeclaringType is not null)
             property = property.DeclaringType.GetProperty(propertyName, declaredFlags)!;
 
-        return property.GetGetMethod()
-            ?? throw new ArgumentException($"Property '{propertyName}' on {objType} does not have a public getter.", nameof(propertyName));
+        return property.GetGetMethod(includeNonPublic)
+            ?? throw new ArgumentException($"Property '{propertyName}' on {objType} does not have a getter with the requested accessibility.", nameof(propertyName));
     }
 
     private static void ValidateObjType(Type objType)
@@ -52,7 +77,7 @@ public static class PropertyInfoX
 
     private static Func<object, object> CompileGetter(GetterCacheKey key)
     {
-        var propertyPathSegments = key.PropertyPath.Split(['.'], StringSplitOptions.TrimEntries);
+        var propertyPathSegments = SplitPropertyPath(key.PropertyPath);
 
         var objParameter = Expression.Parameter(typeof(object));
 
@@ -61,7 +86,7 @@ public static class PropertyInfoX
 
         foreach (string propertyName in propertyPathSegments)
         {
-            var getter = GetGetterMethodOrThrow(currentValueType, propertyName);
+            var getter = GetGetterMethodOrThrow(currentValueType, propertyName, key.IncludeNonPublic);
 
             currentValue = Expression.Property(currentValue, getter);
             currentValueType = getter.ReturnType;
@@ -74,4 +99,7 @@ public static class PropertyInfoX
 
         return lambda.Compile();
     }
+
+    private static string[] SplitPropertyPath(string propertyPath) =>
+        propertyPath.Split(['.'], StringSplitOptions.TrimEntries);
 }
