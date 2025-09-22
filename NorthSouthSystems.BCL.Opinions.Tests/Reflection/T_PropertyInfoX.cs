dbc445621f6e4f5ab755df56a3da410bd643238c
FreeAndWithBeer <30539567+FreeAndWithBeer@users.noreply.github.com>
2025-09-22 11:00:57 -0400

    Added PropertyInfoX with its GetValueCompiled method for improving the performance of repeated reflected access to a property's value.

---
+++ NorthSouthSystems.BCL.Opinions.Tests/Reflection/T_PropertyInfoX.cs
@@ -0,0 +1,81 @@
+﻿// Adapted and simplified from a ChatGPT conversation on 2025-08-21.
+using NorthSouthSystems.Reflection;
+
+public class T_PropertyInfoX
+{
+    private sealed class InputClass
+    {
+        public string StringProperty => "foobar";
+        public int IntProperty => 43;
+        public double? NullableDoubleProperty => 1.23;
+
+        public string NullStringProperty => null;
+        public double? NullDoubleProperty => null;
+
+        internal string NonPublicStringProperty => "nonpublic";
+        public string SetterOnlyStringProperty { set { } }
+    }
+
+    private sealed class InputClassDerived : InputClassBase { }
+
+    protected class InputClassBase { public string StringProperty => "base"; }
+
+    private struct InputStruct { public string StringProperty => "struct"; }
+
+    [Fact]
+    public void GetValueComiled()
+    {
+        object result;
+
+        result = PropertyInfoX.GetValueCompiled(new InputClass(), nameof(InputClass.StringProperty));
+        result.Should().BeOfType<string>().Which.Should().Be(new InputClass().StringProperty);
+
+        result = PropertyInfoX.GetValueCompiled(new InputClass(), nameof(InputClass.IntProperty));
+        result.Should().BeOfType<int>().Which.Should().Be(new InputClass().IntProperty);
+
+        result = PropertyInfoX.GetValueCompiled(new InputClass(), nameof(InputClass.NullableDoubleProperty));
+        result.Should().BeOfType<double>().Which.Should().Be(new InputClass().NullableDoubleProperty);
+
+        result = PropertyInfoX.GetValueCompiled(new InputClass(), nameof(InputClass.NullStringProperty));
+        result.Should().BeNull();
+
+        result = PropertyInfoX.GetValueCompiled(new InputClass(), nameof(InputClass.NullDoubleProperty));
+        result.Should().BeNull();
+
+        result = PropertyInfoX.GetValueCompiled(new InputClassDerived(), nameof(InputClassDerived.StringProperty));
+        result.Should().BeOfType<string>().Which.Should().Be(new InputClassDerived().StringProperty);
+    }
+
+    [Fact]
+    public void GetValueCompiled_Exceptions()
+    {
+        Action act;
+
+        act = static () => PropertyInfoX.GetValueCompiled(null, nameof(InputClass.StringProperty));
+        act.Should().ThrowExactly<ArgumentNullException>().Which.ParamName.Should().Be("obj");
+
+        act = static () => PropertyInfoX.GetValueCompiled(new InputClass(), null);
+        act.Should().ThrowExactly<ArgumentNullException>().Which.ParamName.Should().Be("propertyName");
+
+        act = static () => PropertyInfoX.GetValueCompiled(new InputClass(), string.Empty);
+        act.Should().ThrowExactly<ArgumentException>().Which.ParamName.Should().Be("propertyName");
+
+        act = static () => PropertyInfoX.GetValueCompiled(new InputStruct(), nameof(InputStruct.StringProperty));
+        act.Should().ThrowExactly<ArgumentException>().WithMessage("Value types are not allowed*");
+    }
+
+    [Fact]
+    public void GetGetterMethodOrThrow_Exceptions()
+    {
+        Action act;
+
+        act = static () => PropertyInfoX.GetValueCompiled(new InputClass(), "DoesNotExist");
+        act.Should().ThrowExactly<ArgumentException>();
+
+        act = static () => PropertyInfoX.GetValueCompiled(new InputClass(), nameof(InputClass.NonPublicStringProperty));
+        act.Should().ThrowExactly<ArgumentException>();
+
+        act = static () => PropertyInfoX.GetValueCompiled(new InputClass(), nameof(InputClass.SetterOnlyStringProperty));
+        act.Should().ThrowExactly<ArgumentException>();
+    }
+}
---
+++ NorthSouthSystems.BCL.Opinions/Reflection/PropertyInfoX.cs
@@ -0,0 +1,68 @@
+﻿namespace NorthSouthSystems.Reflection;
+
+using System.Collections.Concurrent;
+using System.Linq.Expressions;
+using System.Reflection;
+
+// Adapted and simplified from a ChatGPT conversation on 2025-08-21.
+public static class PropertyInfoX
+{
+    private static readonly ConcurrentDictionary<GetterCacheKey, Lazy<Func<object, object>>> _getterCache = new();
+    private sealed record GetterCacheKey(Type ObjType, string PropertyName);
+
+    public static object GetValueCompiled(object obj, string propertyName)
+    {
+        ArgumentNullException.ThrowIfNull(obj);
+        ArgumentException.ThrowIfNullOrEmpty(propertyName);
+
+        var objType = obj.GetType();
+        ValidateObjType(objType);
+
+        var cacheKey = new GetterCacheKey(objType, propertyName);
+        var getter = _getterCache.GetOrAdd(cacheKey, ck => new(() => CompileGetter(ck)));
+
+        return getter.Value(obj);
+    }
+
+    public static MethodInfo GetGetterMethodOrThrow(Type objType, string propertyName)
+    {
+        ValidateObjType(objType);
+        ArgumentException.ThrowIfNullOrEmpty(propertyName);
+
+        const BindingFlags flattenFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.FlattenHierarchy;
+        const BindingFlags declaredFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly;
+
+        var property = objType.GetProperty(propertyName, flattenFlags)
+            ?? throw new ArgumentException($"Public property '{propertyName}' not found on {objType}.", nameof(propertyName));
+
+        // We must get the PropertyInfo from the DeclaringType or else Expression.Property will throw:
+        // "System.ArgumentException : The method '...' is not a property accessor (Parameter 'propertyAccessor')"
+        if (property.DeclaringType != objType && property.DeclaringType is not null)
+            property = property.DeclaringType.GetProperty(propertyName, declaredFlags)!;
+
+        return property.GetGetMethod()
+            ?? throw new ArgumentException($"Property '{propertyName}' on {objType} does not have a public getter.", nameof(propertyName));
+    }
+
+    private static void ValidateObjType(Type objType)
+    {
+        if (objType.IsValueType)
+            throw new ArgumentException("Value types are not allowed.", nameof(objType));
+    }
+
+    private static Func<object, object> CompileGetter(GetterCacheKey key)
+    {
+        var objParameter = Expression.Parameter(typeof(object));
+        var objParameterTyped = Expression.Convert(objParameter, key.ObjType);
+
+        var getterMethod = GetGetterMethodOrThrow(key.ObjType, key.PropertyName);
+        Expression getterExpression = Expression.Property(objParameterTyped, getterMethod);
+
+        if (getterMethod.ReturnType.IsValueType)
+            getterExpression = Expression.Convert(getterExpression, typeof(object));
+
+        var getterLambda = Expression.Lambda<Func<object, object>>(getterExpression, objParameter);
+
+        return getterLambda.Compile();
+    }
+}
