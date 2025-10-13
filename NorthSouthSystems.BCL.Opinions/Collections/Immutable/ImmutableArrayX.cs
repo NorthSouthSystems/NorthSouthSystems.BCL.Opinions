e6033e939550bc3e1ad0bcc5da62e24bd9832494
FreeAndWithBeer <30539567+FreeAndWithBeer@users.noreply.github.com>
2025-10-13 08:45:22 -0400

    Added ImmutableArrayX.DefaultToEmpty helper method.

---
+++ NorthSouthSystems.BCL.Opinions/Collections/Immutable/ImmutableArrayX.cs
@@ -0,0 +1,9 @@
+﻿namespace NorthSouthSystems.Collections.Immutable;
+
+using System.Collections.Immutable;
+
+public static class ImmutableArrayX
+{
+    public static ImmutableArray<T> DefaultToEmpty<T>(this ImmutableArray<T> value) =>
+        value.IsDefault ? [] : value;
+}
